27db45336dd493b1936a0d59787d3bf79d8cefac
Caio Oliveira <caioinacio00@gmail.com>
2022-07-02 13:31:15 -0300

    v2

---
+++ Desafio.Bank/Enum/TipoConta.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio.Bank
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+
+        PessoaJuridica = 2
+    }
+}
